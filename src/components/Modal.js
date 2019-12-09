0903df237436f6e558fb8b3923e10b2f557d0b1d
panuwat koktomchai <panuwat_koktomchai@piesoft.net>
2019-12-09 15:06:25 +0700

    set font-size button close modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,7 +18,7 @@ function Modal(props) {
     <div className="w3-modal" style={{display: props.visible ? 'block' : 'none'}}>
       <div className={`w3-clean w3-modal-content w3-animate-${props.animate} w3-card-4`}>
         <header className={`w3-container w3-${props.color}`}>
-          <span onClick={props.setVisible} className="w3-button w3-display-topright">&times;</span>
+          <span onClick={props.setVisible} style={{fontSize: 33}} className="w3-button w3-display-topright">&times;</span>
           <h2>{props.title}</h2>
         </header>
 
