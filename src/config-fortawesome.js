89160a00ee78874d2d0e196d486aaa69d72071ff
panuwat koktomchai <panuwat_koktomchai@piesoft.net>
2019-12-11 12:10:55 +0700

    Home page title

--- src/config-fortawesome.js
+++ src/config-fortawesome.js
@@ -1,8 +1,12 @@
 // Fortawesome Icon
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee, faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 library.add(
+  fab,
   faCheckSquare, 
   faCoffee,
   faCircleNotch,
+  faFacebook,
+  faGithub
   )
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import 'assets/style/Home.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Homepage extends React.Component {
   render() {
     return (
-      <h1>Home page</h1>
+      <div>
+        <div>
+          <div style={{height: 500, textAlign: 'center', backgroundImage:'url(http://localhost:3000/images/book-background.jpg)'}} >
+            <div style={{backgroundColor: 'rgba(0,0,0,0.7)', height: '100%'}}>
+              <div className="me-title" style={{paddingTop: 10}}>
+                <h1 className="w3-text-white">Panuwat Koktomchai</h1>
+                <h2 className="w3-text-white"><b>Software	Developer</b></h2>
+                <img style={{height:250, width: 250, borderRadius: '50%'}} src="http://localhost:3000/images/th.png" alt="th.png"/>
+              </div>
+
+              <div className="me-social w3-margin">
+                <a href="https://www.facebook.com/MarioDeveloper">
+                  <FontAwesomeIcon className="w3-text-white" icon={['fab','facebook']}/>
+                </a>
+                <a href="https://github.com/panuwatkoktomchai">
+                  <FontAwesomeIcon className="w3-text-white" icon={['fab','github']}/>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w3-light-gray" style={{height: 500 }} >
+          </div>
+          </div>
+      </div>
     )
   }
 }
