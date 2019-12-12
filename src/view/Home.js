dd8222dc4e54d203b4c07968aa3aa6cf151348be
panuwat koktomchai <panuwat_koktomchai@piesoft.net>
2019-12-12 11:25:37 +0700

    Hightlight content

--- src/view/Home.js
+++ src/view/Home.js
@@ -31,7 +31,7 @@ class Homepage extends React.Component {
           <div className="w3-row " style={{backgroundColor: 'rgba(0,0,0,0.9)', color: 'white'}}>
             <div className="w3-col m12 l4 w3-padding">
               <div className="me-card-info w3-card-4 w3-border w3-border-black w3-hover-border-orange">
-                <header className="w3-container w3-blue-gray w3-hover-orange w3-text-white">
+                <header className="w3-container w3-blue-gray w3-round-large w3-hover-orange w3-text-white">
                   <h1>{ t('experience.title') }</h1>
                 </header>
 
@@ -61,7 +61,7 @@ class Homepage extends React.Component {
             </div>{/** End class row 12 */}
             <div className="w3-col m12 l4 w3-padding">
               <div className="me-card-info w3-card-4 w3-border w3-border-black w3-hover-border-orange">
-                <header className="w3-container w3-blue-gray w3-hover-orange w3-text-white">
+                <header className="w3-container w3-blue-gray w3-round-large w3-hover-orange w3-text-white">
                   <h1>{ t('skill.title') }</h1>
                 </header>
                 <div className="w3-container me-description">
@@ -99,7 +99,7 @@ class Homepage extends React.Component {
             </div>{/** End class row 12 */}
             <div className="w3-col m12 l4 w3-padding">
               <div className="me-card-info w3-card-4 w3-border w3-border-black w3-hover-border-orange">
-                <header className="w3-container w3-blue-gray w3-hover-orange w3-text-white">
+                <header className="w3-container w3-blue-gray w3-round-large w3-hover-orange w3-text-white">
                   <h1>{ t('training.title') }</h1>
                 </header>
                 <div className="w3-container me-description">
@@ -122,10 +122,33 @@ class Homepage extends React.Component {
             </div> {/** End class row 12 */}
           </div>{/** End block row */}
 
-          <div className="w3-container">
-            <div className="w3-orange">
-              <h1>HIGHLIGHT	PROJECT</h1>
-              <div className="w3-row">
+          <div className="w3-container" style={{backgroundColor: 'rgba(0,0,0,0.9)'}}>
+              <hr className="w3-white"/>
+            <div>
+              <div className="w3-padding w3-center">
+                <h1 className="me-font w3-text-white"><b>Hightlight	Project</b></h1>
+              </div>
+              <div className="w3-row-padding">
+                <div className="w3-card-4 w3-text-white w3-border w3-border-black w3-hover-border-orange w3-round-large w3-col m12 l6 w3-padding">
+                  <div className="w3-center w3-margin">
+                    <img width="200px" src="https://ais.caat.or.th/dist/img/logo-ais.png" alt="ais logo"/>
+                  </div>
+                  <div className="w3-center">
+                    <h2 className="me-font"><u>API</u></h2>
+                    <p className="me-description">	Thailand	Aeronautical	Information	Service	(AIS)	Redesigned	CAAT	website	and	develop	content management	system</p>
+                    <br/>
+                    <div className="me-font"><b>Website :</b></div>
+                    <a className="me-description me-non-underline w3-hover-orange" href="https://ais.caat.or.th/" target="_blank" rel="noopener noreferrer">https://ais.caat.or.th</a>
+                    <br/>
+                    <br/>
+                    <div><b>Tool :</b></div>
+                    <img className="w3-margin" alt="laravel icon" src="./images/laravel.png" width="100px"/>
+                    <img className="w3-margin" alt="mysql icon" src="./images/mysql.png" width="100px"/>
+                    <img className="w3-margin" alt="docker icon" src="./images/docker.png" width="100px"/>
+                    <img className="w3-margin" alt="javascrip icon" src="./images/javascrip.jpg" width="100px"/>
+                  </div>
+                </div>
+
               </div>
             </div>
           </div>
